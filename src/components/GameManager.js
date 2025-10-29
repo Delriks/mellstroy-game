8513181f6f64a4910e6aed08491db35d32e85d68
Vladikonis <vladikonis@gmail.com>
2025-10-30 00:25:31 +0300

    обновил все файлы

--- src/components/GameManager.js
+++ src/components/GameManager.js
@@ -3,6 +3,7 @@ import MoscowCity from './MoscowCity.js'
 import DonatChallenge from './DonatChallenge.js'
 import EscapeDubai from './EscapeDubai.js'
 import EverestChallenge from './EverestChallenge.js'
+import CasinoBoss from './CasinoBoss.js'
  
 export default function GameManager(container) {
   const levels = [
@@ -11,6 +12,7 @@ export default function GameManager(container) {
   { name: 'Donat Challenge', func: DonatChallenge },
   { name: 'Escape to Dubai', func: EscapeDubai },
   { name: 'Everest Challenge', func: EverestChallenge },
+  { name: 'Финальный босс MELLSTROY', func: CasinoBoss },
 ]
 
   let currentLevel = 0
