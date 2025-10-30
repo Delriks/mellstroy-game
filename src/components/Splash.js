9bb9a481fb120e54c583d3a2b3c6995370c6860c
Vladikonis <vladikonis@gmail.com>
2025-10-30 15:15:21 +0300

    пересобрал структуру

--- src/components/Splash.js
+++
@@ -1,9 +0,0 @@
-export default function Splash(container, onNext) {
-  container.innerHTML = `
-    <img src="/logo.png" alt="MELLSTROY Logo" class="logo"/>
-    <h1>MELLSTROY.GAME</h1>
-    <p>🕹️ Добро пожаловать в мир MELLSTROY.GAME </p>
-  `
-
-  setTimeout(onNext, 2500)
-}
